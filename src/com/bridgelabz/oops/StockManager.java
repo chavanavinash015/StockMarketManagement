da78aaa525c2dc572ad1e5bb5450f2f96d948743
arc-015 <chavanavinash015@gmail.com>
2022-10-29 23:26:57 +0530

    Added stock and share check portfolio

--- src/com/bridgelabz/oops/StockManager.java
+++ src/com/bridgelabz/oops/StockManager.java
@@ -5,6 +5,7 @@ public class StockManager {
     public static void main(String[] args) {
         System.out.println("Welcome to the Stock Account Management Program");
 
-
+        StockPortfolio portfolio = new StockPortfolio();
+        portfolio.selectActivity();
     }
 }
