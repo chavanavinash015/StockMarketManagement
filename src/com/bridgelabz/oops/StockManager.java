79c7febc6740526be007169539c69425ffccedcc
arc-015 <chavanavinash015@gmail.com>
2022-10-29 23:11:38 +0530

    Added Stock Market Management

---
+++ src/com/bridgelabz/oops/StockManager.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.oops;
+
+public class StockManager {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Stock Account Management Program");
+
+
+    }
+}
